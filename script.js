2f3e504e05f31c694743e67032c7f2dd57f6a800
BarbaraCristinaNunes <90681011+BarbaraCristinaNunes@users.noreply.github.com>
2021-11-17 11:43:01 +0100

    Create script.js
    Now I am trying to get what the  user is typing. My addEventListener is working because it recognizes that the user is typing but he don't show me what is it typed yet

---
+++ script.js
@@ -0,0 +1,8 @@
+const lake = document.getElementById('lake');
+const tadpole = document.getElementById('tadpole');
+
+window.addEventListener('keydown', test);
+
+function test(event){
+     console.log(event.type);
+}
