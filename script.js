cd1cebcf349631f62db3b1d5440517ab470825f2
BarbaraCristinaNunes <90681011+BarbaraCristinaNunes@users.noreply.github.com>
2021-11-18 11:32:51 +0100

    Update script.js
    Used DOM to access each div that has a layer
    
    Did object for each layer with the properties that I use to change the position of the layers
    
    The function moveSand() change the position of the sands when the user type ArrowLeft or ArrowRight
    
    The MoveFishes() change the position of the fish regardless of the user

--- script.js
+++ script.js
@@ -1,12 +1,41 @@
 const lake = document.getElementById('lake');
 const tadpole = document.getElementById('tadpole');
-let types = [];
-let x = 1400;
-let y = 0;
-let right = 0;
-let left = 0;
+const sand1 = document.getElementById('sand1');
+const sand2 = document.getElementById('sand2');
+const fish1 = document.getElementById('fish1');
+const fish2 = document.getElementById('fish2');
+const dragonfly = document.getElementById('dragonfly');
 
-// console.log(x);
+const positionSand1 = {
+     x: 0,
+};
+
+const positionSand2 = {
+     x: 0,
+};
+
+const tadpolePosition = {
+     x: 1400,
+     y: 0,
+};
+
+const dragonflyPosition = {
+     x: 0,
+     y: 650,
+}
+
+const positionFish1 = {
+     x: 0,
+}
+
+const positionFish2 = {
+     x: 0,
+}
+
+let types = [];
+
+
+console.log(tadpolePosition.x);
 // console.log(y);
 
 window.addEventListener('keydown', (e) => {
@@ -23,7 +52,7 @@ window.addEventListener('keydown', (e) => {
      if(e.key == 'ArrowUp'){
           types.push(e.key)
      }
-     console.log(types);
+     // console.log(types);
 });
 
 window.addEventListener('keyup', (e) => {
@@ -41,66 +70,69 @@ window.addEventListener('keyup', (e) => {
      if(e.key == 'ArrowUp'){
           types.splice('ArrowUp');
      }
-     console.log(types)
+     // console.log(types)
 })
+
+
+function moveSand(){
+     if(types.includes('ArrowLeft')){
+          positionSand1.x += 1;
+          document.getElementById('sand1').style.backgroundPositionX = positionSand1.x + "px";
+     }
+     if(types.includes('ArrowRight')){
+          positionSand1.x -= 1;
+          document.getElementById('sand1').style.backgroundPositionX = positionSand1.x + "px";
+     }
+     if(types.includes('ArrowLeft')){
+          positionSand2.x += 0.5;
+          document.getElementById('sand2').style.backgroundPositionX = positionSand2.x + "px";
+     }
+     if(types.includes('ArrowRight')){
+          positionSand2.x -= 0.5;
+          document.getElementById('sand2').style.backgroundPositionX = positionSand2.x + "px";
+     }
+     // console.log(positionSand2.x + " sand2");
+     // console.log(positionSand1.x + " sand1");
+}
+
+function MoveFishes(){
+     positionFish1.x += 0.4;
+     positionFish2.x -= 0.2;
+
+     fish1.style.backgroundPositionX = positionFish1.x + "px";
+     fish2.style.backgroundPositionX = positionFish2.x + "px";
+}
+
+function moveTadpole(){
+     if(types.includes('ArrowLeft') && tadpolePosition.x < 1400){
+          tadpolePosition.x += 0.5;
+          tadpole.style.right = tadpolePosition.x + "px";
+     }
+     if(types.includes('ArrowRight') && tadpolePosition.x > 0){
+          tadpolePosition.x -= 0.5;
+          tadpole.style.right = tadpolePosition.x + "px";
+     }
+     if(types.includes('ArrowDown') && tadpolePosition.y < 650){
+          tadpolePosition.y += 0.5;
+          tadpole.style.top = tadpolePosition.y + "px";
+     }
+     if(types.includes('ArrowUp') && tadpolePosition.y > 0){
+          tadpolePosition.y -= 0.5;
+          tadpole.style.top = tadpolePosition.y + "px";
+     }
+     if(types.length == 0 && tadpolePosition.y < 650){
+          tadpolePosition.y += 0.5;
+          tadpole.style.top = tadpolePosition.y + "px";
+     }
+
+
+}
+
 
 function game(){
      // moveTadpole();
-     moveSand();
+     // moveSand();
+     // MoveFishes();
      setTimeout(game, 1);
 }
 game();
-
-function moveSand(){
-     if(types.includes('ArrowLeft') && x <= 1400){
-          right += 1;
-          document.getElementById('sand2').style.backgroundPositionX = right + "px";
-     }
-     if(types.includes('ArrowRight') && x > 0){
-          right -= 1;
-          document.getElementById('sand2').style.backgroundPositionX = right + "px";
-     }
-     if(types.includes('ArrowLeft') && x <= 1400){
-          left -= 1;
-          document.getElementById('sand1').style.backgroundPositionX = left + "px";
-     }
-     if(types.includes('ArrowRight') && x > 0){
-          left += 1;
-          document.getElementById('sand1').style.backgroundPositionX = left + "px";
-     }
-}
-// function moveSand2(){
-     
-// }
-
-
-
-function moveTadpole(){
-     if(types.includes('ArrowLeft') && x <= 1400){
-          x += 1;
-          tadpole.style.right = x + "px";
-     }
-     if(types.includes('ArrowRight') && x > 0){
-          x -= 1;
-          tadpole.style.right = x + "px";
-     }
-     if(types.includes('ArrowDown') && y <= 650){
-          y += 1;
-          tadpole.style.top = y + "px";
-     }
-     if(types.includes('ArrowUp') && y > 0){
-          y -= 1;
-          tadpole.style.top = y + "px";
-     }
-     if(types.length == 0){
-          y += 1;
-          tadpole.style.top = y + "px";
-     }
-
-     console.log(x);
-     console.log(y);
-
-}
-// function moveScenary(){
-
-// }
